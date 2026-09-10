79fd4dc6987765353c498c64816131664d69cc8e
viesca272 <45830374+viesca272@users.noreply.github.com>
2026-09-10 03:03:04 -0500

    Add website configuration

---
+++ site-config.js
@@ -0,0 +1,11 @@
+window.ANIMEKAI_SITE = {
+  githubOwner: "viesca272",
+  githubRepo: "AnimeKai-RPC",
+  chromeStoreUrl: "",
+  edgeStoreUrl: "",
+  operaStoreUrl: "",
+  supportUrl: "",
+  issuesUrl: "https://github.com/viesca272/AnimeKai-RPC/issues",
+  stableVersion: "5.0.0",
+  nextVersion: "6.0.0"
+};
