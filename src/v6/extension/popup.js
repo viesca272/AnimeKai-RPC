1f3b71d756bdb4be4863f41d4420a9d094a46a68
viesca272 <45830374+viesca272@users.noreply.github.com>
2026-09-18 04:38:21 -0500

    Update src/v6/extension/popup.js for V6.1.2

--- src/v6/extension/popup.js
+++ src/v6/extension/popup.js
@@ -290,7 +290,7 @@ async function enablePlayerAccess() {
 }
 
 function openSetupRelease() {
-  chrome.tabs.create({url:lastState?.setupUrl || "https://github.com/viesca272/AnimeKai-RPC/releases/tag/v6.1.1"});
+  chrome.tabs.create({url:lastState?.setupUrl || "https://github.com/viesca272/AnimeKai-RPC/releases/tag/v6.1.2"});
 }
 
 $("accent").oninput = e => { $("hex").value = e.target.value; saveAppearance(); };
