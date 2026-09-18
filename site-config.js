9c37ab2a6c121c1d0bc44996dbac83c3017b8392
viesca272 <45830374+viesca272@users.noreply.github.com>
2026-09-18 06:04:02 -0500

    Set website stable version to V6.1.5

--- site-config.js
+++ site-config.js
@@ -6,6 +6,6 @@ window.ANIMEKAI_SITE = {
   operaStoreUrl: "",
   supportUrl: "",
   issuesUrl: "https://github.com/viesca272/AnimeKai-RPC/issues",
-  stableVersion: "6.1.4",
+  stableVersion: "6.1.5",
   nextVersion: "6.2.0"
 };
