e65c378028b675da0f093e869c595466b8048df4
viesca272 <45830374+viesca272@users.noreply.github.com>
2026-09-18 03:57:10 -0500

    Update site-config.js for V6.1.0

--- site-config.js
+++ site-config.js
@@ -6,6 +6,6 @@ window.ANIMEKAI_SITE = {
   operaStoreUrl: "",
   supportUrl: "",
   issuesUrl: "https://github.com/viesca272/AnimeKai-RPC/issues",
-  stableVersion: "5.0.0",
-  nextVersion: "6.0.0"
+  stableVersion: "6.1.0",
+  nextVersion: "6.2.0"
 };
