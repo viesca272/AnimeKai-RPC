4ad0db0c993fab8f358af7bdabeb1e72ad980456
viesca272 <45830374+viesca272@users.noreply.github.com>
2026-09-18 06:30:58 -0500

    Bump src/v6/windows/Install-Helper.ps1 to V6.1.6

--- src/v6/windows/Install-Helper.ps1
+++ src/v6/windows/Install-Helper.ps1
@@ -1,7 +1,7 @@
 param([switch]$Repair)
 $ErrorActionPreference = "Stop"
 
-$Version = "6.1.5"
+$Version = "6.1.6"
 $Channel = "stable"
 $Root = Split-Path -Parent $PSScriptRoot
 $SourceExe = Join-Path $Root "native_host\AnimeKaiRPCNativeHost.exe"
